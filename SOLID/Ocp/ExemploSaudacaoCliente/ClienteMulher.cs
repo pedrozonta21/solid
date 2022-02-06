ebc2779a405050b1209c558f6b9a5fe06b419e29
pedrozonta21 <pedrozonta6@gmail.com>
2022-02-06 01:18:55 -0300

    criando pasta para exemplo da saudacao do cliente

--- SOLID/Ocp/ExemploSaudacaoCliente/ClienteMulher.cs
+++ SOLID/Ocp/ExemploSaudacaoCliente/ClienteMulher.cs
@@ -1,4 +1,4 @@
-﻿namespace SOLID.Ocp
+﻿namespace SOLID.Ocp.ExemploSaudacaoCliente
 {
     public class ClienteMulher : SaudacaoAbstract
     {
--- SOLID/Ocp/ExemploSaudacaoCliente/SaudacaoAbstract.cs
+++ SOLID/Ocp/ExemploSaudacaoCliente/SaudacaoAbstract.cs
@@ -1,4 +1,4 @@
-﻿namespace SOLID.Ocp
+﻿namespace SOLID.Ocp.ExemploSaudacaoCliente
 {
     public abstract class SaudacaoAbstract
     {
